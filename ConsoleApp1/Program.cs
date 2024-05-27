87cd90e3a694c4f72164914137ce42a776a2c584
Timothy Simeone <tim.v.simeone@gmail.com>
2024-05-27 18:48:06 -0500

    created a calculator

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            } else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+            } else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+            Console.ReadLine();
+        }
+    }
+}
